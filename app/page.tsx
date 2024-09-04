8a5416360ff35766a4b1469935a2dee59db89f07
Claudio Oliveira <dev.caon@gmail.com>
2024-09-04 20:34:28 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="text-3xl flex items-center justify-center min-h-screen">
+      <h2 className="font-semibold">Medical App</h2>
+    </section>
+  );
+}
